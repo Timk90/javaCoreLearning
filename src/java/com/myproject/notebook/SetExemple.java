b74f7981b4d05d5e5726a4af78cbb09f06b4173f
Timur Burganov <timk90@mail.ru>
2018-11-05 17:37:30 +0100

    time of Iteration checked (ITERATOR and arrays loop)

--- src/java/com/myproject/notebook/SetExemple.java
+++ src/java/com/myproject/notebook/SetExemple.java
@@ -14,7 +14,7 @@ class SetExemple{
      long start, stop, dt;
      int i;
 
-     // There are three implementations of sets Hash, Tree, Linked
+  // There are three implementations of sets Hash, Tree, LinkedHash
      System.out.println("Work with HashSet:");
      Set<String> hashSet = new HashSet<String>();
      hashSet.addAll((Arrays.asList(new String[]{"1","2","3","4","5","6","7","8","9"})));
@@ -59,7 +59,8 @@ class SetExemple{
      System.out.println("Is A empty "+hashSet.isEmpty());
      System.out.println("Is B empty "+hashSet2.isEmpty());
           
-  /* Starting from this point I will try to assess an average time of common functions 
+  /* 
+     Starting from this point I will try to assess an average time of common functions 
      for different Sets
   */
      hashSet.clear();
@@ -98,9 +99,70 @@ class SetExemple{
      dt = stop - start;
      System.out.println("adding 100000 elements time to TREE Set: "+dt);
      
+  //// Check if the SET contents proper element 
+     //HASH SET
+     start = System.currentTimeMillis();
+     for(i=0; i < 1000000; i++){
+         hashSet.contains(""+i);
+     }
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Search for CONTANTS HASH Set an elements: "+dt);
+
+     //LINKED HASH SET
+     start = System.currentTimeMillis();
+     for(i=0; i < 1000000; i++){
+         linkedSet.contains(""+i);
+     }
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Search for CONTANTS LINKED HASH Set an elements: "+dt);
+
+     //LINKED HASH SET
+     start = System.currentTimeMillis();
+     for(i=0; i < 1000000; i++){
+         treeSet.contains(""+i);
+     }
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Search for CONTANTS TREE Set an elements: "+dt);
      
      //Iterating elements over the whole Set
+  //*** 1). Using iterator ****
+     //Hash SET
+     start = System.currentTimeMillis();
+     Iterator iterator = hashSet.iterator();
+     while(iterator.hasNext()){
+       //System.out.print((String)iterator.next());
+       iterator.next();
+     } 
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Iteration (ITERATOR) over HASH Set takes: "+dt);    
+
+     //Hash Linked SET
+     start = System.currentTimeMillis();
+     iterator = linkedSet.iterator();
+     while(iterator.hasNext()){
+       //System.out.print((String)iterator.next());
+       iterator.next();
+     } 
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Iteration (ITERATOR) over HASH LINKED Set takes: "+dt); 
+
+     //Tree SET
+     start = System.currentTimeMillis();
+     iterator = treeSet.iterator();
+     while(iterator.hasNext()){
+       //System.out.print((String)iterator.next());
+       iterator.next();
+     } 
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Iteration (ITERATOR) over TREE Set takes: "+dt); 
      
+  //*** 2). Using implemented loop ****
      //HASH SET
      start = System.currentTimeMillis();
      for(String str : hashSet){
@@ -139,6 +201,26 @@ class SetExemple{
      stop = System.currentTimeMillis();
      dt = stop - start;
      System.out.println("Removing elements time from HASH Set: "+dt);
+
+     //HASH LINKED SET
+     start = System.currentTimeMillis();
+     for(i=1000000; i > 0; i--)
+     {
+        linkedSet.remove(""+i);
+     }    
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Removing elements time from HASH LINKED Set: "+dt);
+
+     //TREE SET
+     start = System.currentTimeMillis();
+     for(i=1000000; i > 0; i--)
+     {
+        treeSet.remove(""+i);
+     }    
+     stop = System.currentTimeMillis();
+     dt = stop - start;
+     System.out.println("Removing elements time from TREE Set: "+dt);
      
   }
   
